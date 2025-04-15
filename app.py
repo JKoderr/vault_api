e8b1484b86cb44a547b054a17a0c31a040e1597e
DoktorNiczego <neron1051@gmail.com>
2025-04-15 16:12:33 +0200

    user input check

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import string
+import random
+import datetime
+from cryptography.fernet import Fernet
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+#pseudo-random password
+def pass_gen(size, chars=string.ascii_letters + string.digits + string.punctuation):
+        return ''.join(random.choice(chars) for _ in range(size))
 
 @app.route('/', methods = ['GET'])
 def home():
@@ -9,10 +17,14 @@ def home():
 @app.route('/generate-password', methods = ['POST'])
 def generate_password():
     data = request.get_json()
-    length = data['length']
-    service = data['service']
-    print(length, service)
-    return jsonify({"length": length, "service": service})
+
+    if not data or 'length' not in data or 'service' not in data:
+         return jsonify({"error": "Provide 'length' and 'service'"})
+    else:
+        length = data['length']
+        service = data['service']
+        print(length, service)
+        return jsonify({"length": length, "service": service})
 
 
 if __name__ == '__main__':
