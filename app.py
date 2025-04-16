b1e2623fb795f02146e027fd476dbc9443cca194
DoktorNiczego <neron1051@gmail.com>
2025-04-16 10:37:48 +0200

    encryption and saving to file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import string
 import random
 import datetime
+import os
 from cryptography.fernet import Fernet
 from flask import Flask, request, jsonify
 
@@ -18,9 +19,23 @@ def home():
 #path with post method (will not work like default)
 @app.route('/generate-password', methods = ['POST'])
 
+#function that writes key one time
+def load_create_key():
+    if os.path.exists("key.key"):
+        with open("key.key", "rb") as key_file:
+            return key_file.read()
+    else:
+        key = Fernet.generate_key()
+        with open("key.key", "wb") as key_file:  
+            key_file.write(key)
+        return key
+
+
 #function generating password py-json-py
 def generate_password():
-
+    
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    
     data = request.get_json()#fetching json data
 
     if not data or 'length' not in data or 'service' not in data:
@@ -34,7 +49,14 @@ def generate_password():
 
     password = pass_gen(length)#passing length to randomizer
 
-    return jsonify({"status": "success", "password": password}), 201 #showing user pass
+    key = load_create_key()#creating or using existing key
+    cipher = Fernet(key)
+    encrypted_password = cipher.encrypt(password.encode())#encrypting password
+
+    with open("my_passwords.txt", "a") as file:
+        file.write(f"{current_time} | {service} | {encrypted_password}\n")
+
+    return jsonify({"status": "success", "Time": current_time, "password": password, "service": service}), 201 #showing user pass
 
 if __name__ == '__main__':
     app.run(debug=True)
